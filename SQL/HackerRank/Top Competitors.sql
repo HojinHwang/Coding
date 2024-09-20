c8559307031ea3c562c6d97c6d0a569b5988dd50
HOJJIMIN <41909501+HojinHwang@users.noreply.github.com>
2024-09-20 17:44:49 +0900

    Create Top Competitors.sql

---
+++ SQL/HackerRank/Top Competitors.sql
@@ -0,0 +1,19 @@
+SELECT
+     S.hacker_id
+    ,H.name
+FROM Submissions S
+INNER JOIN Challenges C
+ON S.challenge_id = C.challenge_id
+INNER JOIN Difficulty D
+ON C.difficulty_level = D.difficulty_level
+INNER JOIN Hackers H
+ON S.hacker_id = H.hacker_id
+WHERE S.score = D.score
+GROUP BY
+    H.hacker_id
+   ,H.name
+HAVING COUNT(S.hacker_id) > 1
+ORDER BY 
+     COUNT(S.hacker_id) DESC
+    ,H.hacker_id
+;
